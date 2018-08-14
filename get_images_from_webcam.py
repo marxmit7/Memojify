a7a8260dbace052ff6eae321f8cda4415de8ad4f
Amit <amit2rockpower@gmail.com>
2018-08-14 18:01:03 +0530

    added command line argument for labelling

--- get_images_from_webcam.py
+++ get_images_from_webcam.py
@@ -1,10 +1,16 @@
+import argparse
 import cv2
 import numpy as np
 import dlib
 import os
 from imutils import face_utils
 from imutils.face_utils import FaceAligner
-from random import shuffle, randint
+
+ap = argparse.ArgumentParser()
+ap.add_argument( "-l","--label", required=True, help="label of the image to be captured")
+args = vars(ap.parse_args())
+label = args["label"]
+
 
 shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 detector = dlib.get_frontal_face_detector()
@@ -14,7 +20,6 @@ aligned_face = FaceAligner(shape_predictor, desiredFaceWidth=256)
 filepath = os.path.dirname(os.path.abspath(__file__))
 
 frame_counter = 0
-label = input("enter the label of the image to be captured\n")
 img_path = filepath+'/data/'+label
 
 if not os.path.exists(filepath+"/data"):
@@ -27,20 +32,30 @@ vcam = cv2.VideoCapture(0)
 vcam.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
 vcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
 
+x,y,w,h = 40,50,30,40
 
 while True:
 	ret_val , img = vcam.read()
-	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	faces = detector(gray)
+	gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	faces = detector(gray_img)
 
-
+	cv2.rectangle(img, (x, y), (x+w, y+h), (255, 155, 177), 2)
 	cv2.imshow('img', img)
 
+	cv2.imshow('gray_img',gray_img)
 	keypress = cv2.waitKey(1)
 	if keypress%256 == 27:
-		print("escape is pressed quiting...")
+		print("Escape is pressed, quiting...")
 		break
 	elif keypress%256 == 32:
+		if len(faces)>0:
+			face = faces[0]
+			shape_68 = shape_predictor(img,face)
+			shape = face_utils.shape_to_np(shape_68)
+
+
+
+
 		img_name = "{}.png".format(frame_counter)
 		cv2.imwrite(img_path+"/"+ img_name, img)
 		print("{} captured ".format(img_name))
