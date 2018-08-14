1e76cb8cd46def65636b9e1a05359d1a3d731dbb
Amit <amit2rockpower@gmail.com>
2018-08-14 16:13:34 +0530

    adding face aligner

---
+++ get_images_from_webcam.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import dlib
+import os
+from imutils import face_utils
+from imutils.face_utils import FaceAligner
+from random import shuffle, randint
+
+shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+detector = dlib.get_frontal_face_detector()
+
+vcam = cv2.VideoCapture(0)
+vcam.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+vcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
+
+aligned_face = FaceAligner(shape_predictor, desiredFaceWidth=256)
+filepath = os.path.dirname(os.path.abspath(__file__))
+
+
+if not os.path.exists("data"):
+    os.mkdir('data')
+
+frame_counter = 0
