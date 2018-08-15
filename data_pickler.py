a9c859c7f46c6292c0f8b497cdd386621c70bc3c
Amit <amit2rockpower@gmail.com>
2018-08-15 12:13:32 +0530

    shuffling all the numpy arrays of imges

--- data_pickler.py
+++ data_pickler.py
@@ -5,26 +5,38 @@ import random
 from sklearn.utils import shuffle
 import numpy as np
 
+dataset_path = os.path.dirname(os.path.abspath(__file__))+'/data'
 
 def images_labels(data_path):
-    images =[]
+    images = []
     labels = []
+    image_label=[]
+
     for folder in os.listdir(data_path):
         if not folder.startswith('.'):
             file_path = data_path+'/'+folder
             for image in os.listdir(file_path):
                 img = cv2.imread(file_path+'/'+image,0)
-                images.append(np.array(img,dtype = np.float16))
-                labels.append(str(folder))
+                image_label.append((np.array(img,dtype = np.float16),str(folder)))
+
+            shuffle_me= shuffle(shuffle(shuffle(image_label)))
+
+            for image, label in shuffle_me:
+                images.append(image)
+                labels.append(label)
+        print(len(images),len(labels))
+
     return images,labels
 
-dataset_path = os.path.dirname(os.path.abspath(__file__))+'/data'
 
 images ,labels = images_labels(dataset_path)
-print(len(images),len(labels))
 
 for image,label in zip(images,labels):
-    print(image,label,"\n")
+    print(image,label)
+
+
+
+
 
 
 
