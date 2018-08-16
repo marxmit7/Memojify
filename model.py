9bb16251d56edda1869b5629b251a779b33038b4
Amit <amit2rockpower@gmail.com>
2018-08-17 02:21:05 +0530

    got some accuracy of 80%  with current

--- model.py
+++ model.py
@@ -1,14 +1,15 @@
 import keras
 from keras.models import Sequential
 from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Conv2D,MaxPooling2D
+from keras.layers.convolutional import Conv2D
 from keras.layers.core import Activation , Dense , Dropout , Flatten
+from keras.layers import AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D, GlobalAveragePooling2D
 from keras import backend as K
 import numpy as np
 from keras.utils import np_utils
 from keras import optimizers
 from keras.layers import BatchNormalization
-from keras.callbacks import  ModelCheckpoint
+from keras.callbacks import ModelCheckpoint
 from keras.models import load_model
 from utils import get_data , get_no_of_classes , get_image_size
 import matplotlib.pyplot as plt
@@ -23,6 +24,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument( "-e","--epochs", required=True, help="enter the no of epochs")
+
 args = vars(ap.parse_args())
 
 dataset_path = os.path.dirname(os.path.abspath(__file__))+'/data'
@@ -41,27 +43,21 @@ conv2_size = 2
 pool_size = 2
 
 
-def cnn_model():
-    model = Sequential()
-    model.add(Conv2D(nb_filters1, (conv1_size,conv1_size), input_shape=(input_size[0], input_size[1], 1),activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(pool_size, pool_size),strides=(2, 2), padding='same'))
-
-    model.add(Conv2D(nb_filters2, (conv2_size, conv2_size)))
-    model.add(Activation("relu"))
-    model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))
-
-    model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(Activation("relu"))
-    model.add(Dropout(0.5))
-    model.add(Dense(no_classes, activation='softmax'))
-    sgd = optimizers.SGD(lr = 1e-2)
-    model.compile(loss='categorical_crossentropy',
-                optimizer=sgd,
-                metrics=['accuracy'])
-    return model
-
+def keras_model():
+	model = Sequential()
+	model.add(Conv2D(32, (5,5), input_shape=(input_size[0], input_size[1], 1), activation='relu'))
+	model.add(BatchNormalization())
+	model.add(MaxPooling2D(pool_size=(10, 10), strides=(10, 10), padding='same'))
+	model.add(Flatten())
+	model.add(Dense(1024, activation='relu'))
+	model.add(BatchNormalization())
+	model.add(Dropout(0.6))
+	model.add(Dense(no_classes, activation='softmax'))
+	sgd = optimizers.SGD(lr=1e-2)
+	model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+	checkpoint1 = ModelCheckpoint(model_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+	callbacks_list = [checkpoint1]
+	return model, callbacks_list
 
 def train():
     train_images,train_labels,test_images,test_labels,val_images,val_labels = get_data()
@@ -74,20 +70,20 @@ def train():
     test_labels = np_utils.to_categorical(test_labels)
     val_labels = np_utils.to_categorical(val_labels)
 
+    model,callbacks_list  = keras_model()
 
-    model  = cnn_model()
-    history = model.fit(train_images,train_labels,validation_data=(val_images,val_labels),epochs=epochs,batch_size=batch_size)
-    model.save(model_path)
-    model.save_weights(weight_path)
+    history = model.fit(train_images,train_labels,validation_data=(val_images,val_labels),epochs=epochs,batch_size=batch_size,callbacks=callbacks_list)
 
 
-    model = load_model(model_path)
-
-    (eval_loss, eval_accuracy) = model.evaluate(test_images,test_labels,batch_size=batch_size,verbose=1)
-
+    (eval_loss, eval_accuracy) = model.evaluate(test_images,test_labels,batch_size=batch_size,verbose=0)
 
     print("accuracy : {:.2f}%".format(eval_accuracy * 100))
     print("loss : {}".format(eval_loss))
+
+    model.save(model_path)
+    model.save_weights(weight_path)
+    model = load_model(model_path)
+
 
     plt.figure(1)
     plt.subplot(211)
